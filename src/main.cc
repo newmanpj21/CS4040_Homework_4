330d9bc2261187b25d919a5c242c5cdd5ad79f20
pete-desktop <pjn@lawsononline.net>
2023-11-02 14:15:50 -0400

    created main source file

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+/**
+ * @file main.cc
+ * @brief Main file for HW4
+ * @author Pete Newman
+ * @date 11/02/2023
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// Main function
+int main()
+{
+}
