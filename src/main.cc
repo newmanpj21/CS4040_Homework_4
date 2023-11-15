b77701448569b320d88183aaa2dba776ba6c1214
pete-desktop <pjn@lawsononline.net>
2023-11-14 22:10:45 -0500

    fixed the rod cutting alg and implemented the basic coin change alg

--- src/main.cc
+++ src/main.cc
@@ -33,6 +33,15 @@ int memoizedCutRod(vector<int> p, int n);
 int memoizedCutRodAux(vector<int> p, int n, vector<int> &r);
 
 /**
+ * @brief function to calculate the minimum number of coins to make change for x
+ *
+ * @param x is the amount of change to make
+ * @param coins is the vector of coin denominations
+ * @return int
+ */
+long long int calcCoinChange(int x, vector<int> &coins);
+
+/**
  * @brief Main function for HW4
  */
 int main()
@@ -51,8 +60,13 @@ int main()
     p[12] = 36;
     p[16] = 48;
 
-    int n = 4096;
+    int n = 64;
     cout << "Max revenue for rod of length " << n << " is " << memoizedCutRod(p, n) << endl;
+
+    // this section tests the coin change problem
+    vector<int> coins = {1, 5, 10, 25, 50, 100, 200, 500, 1000, 2000};
+    int x = 5000;
+    cout << "Number of ways to make change for " << x << " cents is " << calcCoinChange(x, coins) << endl;
     return 0;
 }
 
@@ -77,15 +91,33 @@ int memoizedCutRodAux(vector<int> p, int n, vector<int> &r)
     else
     {
         int q = -1;
-        for (int i = 1; i <= n; i++)
+        for (int i = min(n, (int)p.size() - 1); i >= 1; i--)
         {
             if (p[i] == -1)
             {
                 continue;
             }
-            q = max(q, p[i] + memoizedCutRodAux(p, n - i, r));
+            if (n - i >= 0)
+            {
+                q = max(q, p[i] + memoizedCutRodAux(p, n - i, r));
+            }
         }
         r[n] = q;
         return q;
     }
+}
+
+long long int calcCoinChange(int x, vector<int> &coins)
+{
+    vector<long long int> ways(x + 1, 0);
+    ways[0] = 1;
+    for (int i = 0; i < coins.size(); i++)
+    {
+        int coinValue = coins[i];
+        for (int j = coinValue; j <= x; j++)
+        {
+            ways[j] = ways[j - coinValue] + ways[j];
+        }
+    }
+    return ways[x];
 }
